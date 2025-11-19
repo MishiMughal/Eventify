e2e39af3984535efa5e2ba56136b6657fc813499
zain100000 <muhammadzainulabideen292@gmail.com>
2025-11-19 23:44:14 +0500

    Comment out local BASE_URL for clarity in CONFIG.jsx

--- eventify-user/src/redux/config/Config.jsx
+++ eventify-user/src/redux/config/Config.jsx
@@ -1,5 +1,5 @@
 const CONFIG = {
-  BASE_URL: 'http://192.168.1.9:8000/api',
+  // BASE_URL: 'http://192.168.1.9:8000/api',
   // BASE_URL: 'https://eventify-backend-mu.vercel.app/api',
 
   // When using android emulator and using backend locally, use the below line
