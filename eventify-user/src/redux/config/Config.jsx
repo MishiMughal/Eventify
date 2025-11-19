a46e63c101bcdad90eb1747148b07fa971fa0210
zain100000 <muhammadzainulabideen292@gmail.com>
2025-11-19 23:39:48 +0500

    Update CONFIG.js comments for clarity on BASE_URL usage
    eventify-user

--- eventify-user/src/redux/config/Config.jsx
+++ eventify-user/src/redux/config/Config.jsx
@@ -1,6 +1,13 @@
 const CONFIG = {
-  BASE_URL: 'http://192.168.1.9:8000/api',
+  // When using physical device and using backend locally, use the local network IP address of your machine
+  // open cmd and type ipconfig to find it ipv4 address
+  // BASE_URL: 'http://192.168.1.9:8000/api',
+
+  // When using live backend server, uncomment the below line and comment the above line
   // BASE_URL: 'https://eventify-backend-mu.vercel.app/api',
+
+  // When using android emulator and using backend locally, use the below line
+  BASE_URL: 'http://10.10.2.2:8000/api',
 };
 
 export default CONFIG;
