ab4c52c9cbfb4c9f0b32beadc3fa7ab8b9d10085
zain100000 <muhammadzainulabideen292@gmail.com>
2025-11-19 23:41:58 +0500

    Fix BASE_URL for Android emulator in CONFIG.jsx

--- eventify-user/src/redux/config/Config.jsx
+++ eventify-user/src/redux/config/Config.jsx
@@ -3,7 +3,7 @@ const CONFIG = {
   // BASE_URL: 'https://eventify-backend-mu.vercel.app/api',
 
   // When using android emulator and using backend locally, use the below line
-  BASE_URL: 'http://10.10.2.2:8000/api',
+  BASE_URL: 'http://10.0.2.2:8000/api',
 };
 
 export default CONFIG;
