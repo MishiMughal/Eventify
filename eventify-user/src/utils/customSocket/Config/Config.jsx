94a3c222b8df7961bf3ed4939f6157f154001648
zain100000 <muhammadzainulabideen292@gmail.com>
2026-01-20 09:48:24 +0500

    fixed

--- eventify-user/src/utils/customSocket/Config/Config.jsx
+++ eventify-user/src/utils/customSocket/Config/Config.jsx
@@ -1,6 +1,7 @@
 const CONFIG = {
-  SOCKET_URL: 'http://192.168.1.9:8000',
-  // SOCKET_URL: 'https://coffee-spot-backend.onrender.com',
+  // SOCKET_URL: 'http://192.168.1.9:8000', //when using real mobile device
+  SOCKET_URL: 'http://10.0.2.2:8000', // when using android emulator
+  // SOCKET_URL: 'https://coffee-spot-backend.onrender.com', //when using deployed backend
 };
 
 export default CONFIG;
